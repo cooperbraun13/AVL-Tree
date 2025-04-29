3a40fc749815e72dcf5a0b32faf5c851c8c02ede
cooperbraun13 <cooperjbraun13@gmail.com>
2025-04-29 14:20:00 -0700

    added node files and implemented avl node header file

---
+++ avl_node.hpp
@@ -0,0 +1,15 @@
+#ifndef AVL_NODE_HPP
+#define AVL_TREE_HPP
+
+class AVLNode {
+    public:
+        int value;
+        AVLNode* left;
+        AVLNode* right;
+        AVLNode* parentl;
+        int height;
+        AVLNode(int val);
+        ~AVLNode();
+};
+
+#endif
